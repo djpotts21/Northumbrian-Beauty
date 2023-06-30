99bb6ca41923f17e6e262163226a2fbd4b79e256
Daniel Potts <djpotts2891@gmail.com>
2023-06-30 20:29:56 +0000

    Seperate Data in to own JS for later additions

---
+++ assets/js/placesdata.js
@@ -0,0 +1,25 @@
+var lovelyPlacesNorthumberland = [
+    { 
+      position: {lat: 55.18559938889182, lng: -1.5042083978634628},
+      title: "Newbiggin Maritime Centre",
+      website: "http://www.nmcentre.org.uk/",
+      blurb: `We are the gateway to Northumberland's coastline. Discover life in a historic fishing village. Discover the Mary Joicey all weather lifeboat and the Girl Anne fishing coble. Visit the 65 seat Breakwater Café serving local food and overlooking the bay. We are an all weather attraction, open all year.`, 
+      image: "https://d2f0ora2gkri0g.cloudfront.net/bkpam2155390_img_0200.jpg",
+    },
+    { 
+      position: {lat: 55.19021055122221, lng: -1.547346828276308},
+      title: "Woodhorn Museum",
+      website: "https://museumsnorthumberland.org.uk/woodhorn-museum/", 
+      blurb: `Enjoy our excellent facilities, cutting-edge arts and cultural exhibits and a fun-filled events programme. Located in what was once the largest pit village in the world, the former colliery is now home to our popular visitor attraction, welcoming thousands of people each year.`, 
+      image: "https://d3d00swyhr67nd.cloudfront.net/w800h800/DSC_2701-1024x683.jpg",
+    },
+    { 
+        position: {lat: 55.34591765170579, lng: -1.6119417119583808},
+        title: "Warkworth Castle",
+        website: "https://www.english-heritage.org.uk/visit/places/warkworth-castle-and-hermitage/?utm_source=Google%20Business&utm_campaign=Local%20Listings&utm_medium=Google%20Business%20Profiles&utm_content=warkworth%20castle", 
+        blurb: `Striking new sculptures have been installed in the castle grounds to create a series of interactive trails which reveal the lives of people who lived in or near the castle in the early 15th century. Choose your character, from Henry Percy of Atholl to Eleanor Neville: Countess of Northumberland to John del Warderobe: long-serving servant of the rulers of Warkworth, to lead you round the castle.`, 
+        image: "https://www.visitnorthumberland.com/VisitNorthumberland/media/VisitNorthumberland/Home/Explore/Destinations/Market%20Towns/Warkworth/warkworth-2023.jpg?ext=.jpg",
+    }
+  ]
+
+  export {lovelyPlacesNorthumberland}
