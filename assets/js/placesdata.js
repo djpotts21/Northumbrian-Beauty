b656c68cc05439630183a4816db7f9b05d28008e
Daniel Potts <djpotts2891@gmail.com>
2023-06-30 21:30:40 +0000

    Added more places to dataset

--- assets/js/placesdata.js
+++ assets/js/placesdata.js
@@ -14,11 +14,25 @@ var lovelyPlacesNorthumberland = [
       image: "https://d3d00swyhr67nd.cloudfront.net/w800h800/DSC_2701-1024x683.jpg",
     },
     { 
+        position: {lat: 55.29369, lng: -1.5840619},
+        title: "Duridge Bay & Country Park",
+        website: "https://www.northumberland.gov.uk/Local/Country-parks-visitor-centres-coastal-sitesPubl/Druridge-Bay-Country-Park-visitor-centre.aspx", 
+        blurb: `Infringed with rugged dunes, Druridge Bay has unspoilt coastline stretching as far as the eye can see. The area is a haven for birdwatchers, walkers and anyone wanting to breathe in the natural beauty of the Northumberland coast in a peaceful setting.`, 
+        image: "https://images.visitnorthumberland.com/Druridge-Links/vn-large-Aerial-shot-of-Druridge-Bay.jpg",
+    },
+    { 
         position: {lat: 55.34591765170579, lng: -1.6119417119583808},
         title: "Warkworth Castle",
         website: "https://www.english-heritage.org.uk/visit/places/warkworth-castle-and-hermitage/?utm_source=Google%20Business&utm_campaign=Local%20Listings&utm_medium=Google%20Business%20Profiles&utm_content=warkworth%20castle", 
         blurb: `Striking new sculptures have been installed in the castle grounds to create a series of interactive trails which reveal the lives of people who lived in or near the castle in the early 15th century. Choose your character, from Henry Percy of Atholl to Eleanor Neville: Countess of Northumberland to John del Warderobe: long-serving servant of the rulers of Warkworth, to lead you round the castle.`, 
         image: "https://www.visitnorthumberland.com/VisitNorthumberland/media/VisitNorthumberland/Home/Explore/Destinations/Market%20Towns/Warkworth/warkworth-2023.jpg?ext=.jpg",
+    },
+    { 
+        position: {lat: 55.41569608405353, lng: -1.7059520734667566},
+        title: "Alnwick Castle & Alnwick Garden",
+        website: "https://www.alnwickcastle.com/", 
+        blurb: `The iconic Alnwick Castle is a Northumberland filming location for Harry Potter, Downton Abbey and more. Find out about its exciting history and visit.`, 
+        image: "https://thirdeyetraveller.com/wp-content/uploads/NORTHUMBERLAND-14-of-18.jpg",
     }
   ]
 
