24f921cd5073175d0df2b0d2ace77be935619c4a
Daniel Potts <djpotts2891@gmail.com>
2023-07-04 22:09:51 +0000

    Added sidebar list with toggle to show/hide

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,33 +2,36 @@ import {lovelyPlacesNorthumberland} from "./placesdata.js"
 
 var locations = []
 
-  function showIntroGuide() {
-    var introDiv = document.getElementById("intro-guide");
-    introDiv.classList.add("show");
-    introDiv.classList.remove("hide");
-    var mapDiv = document.getElementById("map");
-    mapDiv.classList.add("intro-visable");
+  function toggleSidebarRight() {
+    if (document.getElementById("show-as-list-toggle").classList.contains("show-list")){
+      document.getElementById("map").style.width = '60%';
+      document.getElementById("show-as-list-toggle").innerText = "Hide List";
+      document.getElementById("show-as-list-toggle").classList.remove("show-list")
+      document.getElementById("show-as-list-toggle").classList.add("hide-list")
+    }
+    else {
+      document.getElementById("map").style.width = '100%';
+      document.getElementById("show-as-list-toggle").innerText = "Show as a List";
+      document.getElementById("show-as-list-toggle").classList.remove("hide-list")
+      document.getElementById("show-as-list-toggle").classList.add("show-list")
   }
-  function hideIntroGuide() {
-    var introDiv = document.getElementById("intro-guide");
-    introDiv.classList.remove("show");
-    introDiv.classList.add("hide");
-    var mapDiv = document.getElementById("map");
-    mapDiv.classList.remove("intro-visable");
-  }
+}
+ 
 
   function clearLocations() {
+    document.getElementById("sidebar-right").innerHTML = ""
     locations = []; 
     initMap();
   }
 
   function lovelyPlacesNorthumberlandFunc() {
-    hideIntroGuide()
+    document.getElementById("sidebar-right").innerHTML = ""
     locations = lovelyPlacesNorthumberland; 
     initMap();
   }
 
-  document.getElementById("home").addEventListener("click", showIntroGuide);
+  
+  document.getElementById("show-as-list-toggle").addEventListener("click", toggleSidebarRight);
   document.getElementById("nice-places").addEventListener("click", lovelyPlacesNorthumberlandFunc);
   document.getElementById("clear").addEventListener("click", clearLocations);
 
@@ -44,6 +47,7 @@ var locations = []
     });
 
     const infoWindow = new InfoWindow();
+
 
     locations.forEach(({ position, title, website, blurb, image }, i) => {
       const pin = new PinElement({
@@ -73,6 +77,51 @@ var locations = []
           );
         infoWindow.open(marker.map, marker);
       });
+    });
+        locations.forEach(({ position, title, website, blurb, image }, i) => {
+      const pin = new PinElement({
+        glyph: `${i + 1}`,
+      });
+      const marker = new AdvancedMarkerElement({
+        position,
+        map,
+        title: `${title}`,
+        content: pin.element,
+      });
+
+      marker.addListener("click", ({ domEvent, latLng }) => {
+        
+        const { target } = domEvent;
+
+        infoWindow.close();
+        infoWindow.setContent(`
+          <div id="info-window-left">
+          <p><strong>Attraction Name: </strong>${title}<br></p>
+          <p><strong>Website: </strong><a href="${website}" alt="Link to ${title}'s website" target="_blank">Click here to view website</a><br></p>
+          <p><strong>Description: </strong>${blurb}<br></p>
+          </div>
+          <div id="info-window-right">
+          <img src="${image}" alt="Photo of ${title}">
+          </div>`
+          );
+        infoWindow.open(marker.map, marker);
+      });
+      
+      document.getElementById("sidebar-right").innerHTML += `
+          <div id="list-item">
+            <div id="list-content-text">
+              <p><strong>${i+1}</strong><br></p>
+              <p><strong>Attraction Name: </strong>${title}<br></p>
+              <p><strong>Website: </strong><a href="${website}" alt="Link to ${title}'s website" target="_blank">Click here to view website</a><br></p>
+              <p><strong>Description: </strong>${blurb}<br></p>
+            </div>
+            <div id="list-content-image">
+             <img src="${image}" alt="Photo of ${title}">
+            </div>
+          </div>
+          
+      `
+
     });
   }
   initMap();
