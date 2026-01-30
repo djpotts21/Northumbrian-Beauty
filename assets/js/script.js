14e33587b4b5364af46190af6bc561bcb1ae2814
Daniel Potts <131493188+djpotts21@users.noreply.github.com>
2026-01-30 16:56:49 +0000

    Update API key in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,7 +30,7 @@
     };
   d[l] ? console.warn(p + " only loads once. Ignoring:", g) : d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n));
 })({
-  key: "AIzaSyBW6UV4AjvEdOxJJJy-ywVB4tlxab0AUjI",
+  key: "AIzaSyD5glleTHLXTgAg5KpvydyZTSjdX4OpsDY",
   v: "weekly",
   // Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
   // Add other bootstrap parameters as needed, using camel case.
